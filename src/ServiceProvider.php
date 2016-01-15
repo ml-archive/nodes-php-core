0973cf5bd34d520b0db6adad97dd9cd1ce31ee1f
Morten Rugaard <moru@nodes.dk>
2016-01-15 17:15:49 +0100

    Added browscap workaround. Added Nodes user agent support

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,6 +1,9 @@
 <?php
 namespace Nodes;
 
+use Nodes\Support\UserAgent\Parser as NodesUserAgentParser;
+use phpbrowscap\Browscap;
+
 /**
  * Class ServiceProvider
  *
@@ -36,7 +39,43 @@ class ServiceProvider extends AbstractServiceProvider
     {
         parent::register();
 
-        // Register Artisan commands
-        $this->commands($this->commands);
+        $this->registerBrowscap();
+        $this->registerUserAgentParser();
+    }
+
+    /**
+     * Register browscap
+     *
+     * @author Morten Rugaard <moru@nodes.dk>
+     *
+     * @access public
+     * @return void
+     */
+    protected function registerBrowscap()
+    {
+        $this->app->singleton(Browscap::class, function($app) {
+            $browscap = new Browscap(storage_path('framework/cache'));
+            $browscap->remoteIniUrl = 'http://browscap.org/stream?q=PHP_BrowsCapINI';
+            return $browscap;
+        });
+    }
+
+    /**
+     * Register user agent parser
+     *
+     * @author Morten Rugaard <moru@nodes.dk>
+     *
+     * @access protected
+     * @return void
+     */
+    protected function registerUserAgentParser()
+    {
+        $this->app->bind('nodes.useragent', function($app) {
+            return new NodesUserAgentParser($app['request']);
+        });
+
+        $this->app->singleton(NodesUserAgentParser::class, function($app) {
+            return $app['nodes.useragent'];
+        });
     }
 }
